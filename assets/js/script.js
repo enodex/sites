424f7d7c539beddd91eae3b44f1b044a9d363897
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-09 01:55:22 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,19 +59,19 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | DevAshu";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Enodex";
+            $("#favicon").attr("href", "assets/images/revisi.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            document.title = "Come Back";
+            $("#favicon").attr("href", "assets/images/revisi.png");
         }
     });
 
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend developer", "backend developer", "web designer", "UI/UX developer", "web developer","Graphics Designer"],
+    strings: ["Staking Service Provider", "IBC Relayer",""],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
