5d50671cf8230dc66701b5dff100e6ab1651164f
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-10 00:15:02 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,7 @@ function showSkills(skills) {
               <div class="info">
                 <img src=${skill.icon} alt="skill" height="45%" width="45%" />
                 <a class="btn" href=${skill.url} style="color:#FFFFFF;"><span>${skill.name}</span></a>
-            <a href="${skill.url}" class="" style="color:#FFFFFF; "><span>Get In <i class="fas fa-code"></i></span></a>
+            <a href="${skill.url}" class="bv" "><span>Get In <i class="fas fa-code"></i></span></a>
               </div>
             </div>
             `
