66c7584666e78c76bbf951a247f0d50794ec94f4
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-09 22:34:10 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,7 +96,7 @@ function showSkills(skills) {
         skillHTML += `
         <div class="bar">
               <div class="info">
-                <img src=${skill.icon} alt="skill" />
+                <img src=${skill.icon} alt="skill" height="50%" width="50%" />
                 <span>${skill.name}</span>
               </div>
             </div>`
