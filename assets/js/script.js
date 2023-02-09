b5e4927130980d2b9aaa2add8bcc7f02160db7d7
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-10 00:46:54 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,7 @@ var typed = new Typed(".typing-text", {
 async function fetchData(type = "skills") {
     let response
     type === "skills" ?
-        response = await fetch("skills.json")
+        response = await fetch("mainnet.json")
         :
         response = await fetch("./projects/projects.json")
     const data = await response.json();
